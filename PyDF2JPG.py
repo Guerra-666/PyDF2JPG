52c2200d42a3e2b1c2fbbd50e62cc14e5501e865
Adrian Guerra <142639062+Guerra-666@users.noreply.github.com>
2023-12-03 18:31:25 -0600

    Update PyDF2JPG.py
    Se añade interfaz de usuario
    Se añade barra de progreso
    Seleccion de carpeta Origen/Destino

--- PyDF2JPG.py
+++ PyDF2JPG.py
@@ -1,29 +1,80 @@
+import os
+from tkinter import Tk, Label, Button, filedialog, Frame, ttk
 from pdf2image import convert_from_path
 from PIL import Image
-import os
 
-# Directorio de entrada donde se encuentran los archivos PDF
-input_dir = '/Users/tu_usuario/Desktop/certificados_pdf/'  # Cambia 'directorio_de_entrada/' por tu carpeta de entrada
-# Directorio de salida donde se guardarán las imágenes JPG
-output_dir = '/Users/tu_usuario/Desktop/imagenes_certificados/'  # Cambia 'directorio_de_salida/' por tu carpeta de salida
+class PDFConverterApp:
+    def __init__(self, master):
+        self.master = master
+        master.title("PyDF to JPG Converter")
 
-# Crea el directorio de salida si no existe
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+        self.input_dir = ""
+        self.output_dir = ""
 
-# Lista todos los archivos en el directorio de entrada
-pdf_files = [file for file in os.listdir(input_dir) if file.endswith('.pdf')]
+        self.frame1 = Frame(master)
+        self.frame1.pack()
 
-# Convierte cada archivo PDF en imágenes JPG
-for pdf_file in pdf_files:
-    pdf_path = os.path.join(input_dir, pdf_file)
-    images = convert_from_path(pdf_path)
-    
-    # Guarda cada página como un archivo JPG en el directorio de salida
-    for i, image in enumerate(images):
-        jpg_path = os.path.join(output_dir, f'{os.path.splitext(pdf_file)[0]}_pagina_{i + 1}.jpg')
-        image.save(jpg_path, 'JPEG')
+        self.label1 = Label(self.frame1, text="Selecciona la carpeta de entrada:")
+        self.label1.pack(side="left")
 
-    print(f'Se han convertido {len(images)} páginas de "{pdf_file}" a imágenes JPG.')
+        self.input_button = Button(self.frame1, text="Seleccionar Carpeta", command=self.select_input_folder)
+        self.input_button.pack(side="left", padx=(5, 0))
 
-print('La conversión de PDF a JPG se ha completado.')
+        self.frame2 = Frame(master)
+        self.frame2.pack()
+
+        self.label2 = Label(self.frame2, text="Selecciona la carpeta de salida:")
+        self.label2.pack(side="left")
+
+        self.output_button = Button(self.frame2, text="Seleccionar Carpeta", command=self.select_output_folder)
+        self.output_button.pack(side="left", padx=(5, 0))
+
+        self.convert_button = Button(master, text="Convertir PDFs", command=self.convert_pdfs)
+        self.convert_button.pack(pady=(10, 0))
+
+        self.progress_bar = ttk.Progressbar(master, orient="horizontal", length=300, mode="determinate")
+        self.progress_bar.pack(pady=(10, 0))
+
+    def select_input_folder(self):
+        self.input_dir = filedialog.askdirectory()
+        print(f"Directorio de entrada seleccionado: {self.input_dir}")
+        self.show_confirmation(self.frame1)
+
+    def select_output_folder(self):
+        self.output_dir = filedialog.askdirectory()
+        print(f"Directorio de salida seleccionado: {self.output_dir}")
+        self.show_confirmation(self.frame2)
+
+    def convert_pdfs(self):
+        if not self.input_dir or not self.output_dir:
+            print("Selecciona directorios de entrada y salida antes de convertir.")
+            return
+
+        pdf_files = [file for file in os.listdir(self.input_dir) if file.lower().endswith('.pdf')]
+        total_pages = sum(len(convert_from_path(os.path.join(self.input_dir, pdf_file))) for pdf_file in pdf_files)
+
+        self.progress_bar["maximum"] = total_pages
+        self.progress_bar["value"] = 0
+
+        for pdf_file in pdf_files:
+            pdf_path = os.path.join(self.input_dir, pdf_file)
+            images = convert_from_path(pdf_path)
+
+            for i, image in enumerate(images):
+                jpg_path = os.path.join(self.output_dir, f'{os.path.splitext(pdf_file)[0]}_pagina_{i + 1}.jpg')
+                image.save(jpg_path, 'JPEG')
+                self.progress_bar["value"] += 1
+                self.master.update_idletasks()  # Actualiza la interfaz gráfica
+
+            print(f'Se han convertido {len(images)} páginas de "{pdf_file}" a imágenes JPG.')
+
+        print('La conversión de PDF a JPG se ha completado.')
+
+    def show_confirmation(self, frame):
+        confirmation_label = Label(frame, text=" ✓ ", fg="green")
+        confirmation_label.pack(side="left")
+
+if __name__ == "__main__":
+    root = Tk()
+    app = PDFConverterApp(root)
+    root.mainloop()
